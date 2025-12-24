0ffa5b5e7eeeb732758a588d00a716eceb50e245
Goswami <2374796@cognizant.com>
2025-12-24 23:11:52 +0530

    Java Basics new code addition

--- src/Java_Basics/Test.java
+++ src/Java_Basics/Test.java
@@ -3,5 +3,41 @@ package Java_Basics;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        int a = 56;
+        System.out.println(a);
+        a=20;
+        System.out.println(a);
+        String name = "John";
+        System.out.println(name);
+
+
+        int i =50;
+        double d = i;//implicit type casting
+        System.out.println(i);
+        System.out.println(d);
+
+        double myval = 34.22;
+        int myint = (int)myval;//explicit type casting
+        System.out.println(myval + " and " + myint);
+
+        int m = 81;
+
+        if (m > 90) {
+            System.out.println("Grade-A");
+        } else  if ( m > 80 && m <=90) {
+            System.out.println("Grade-B");
+        } else {
+            System.out.println("fail");
+        }
+
+        int n = 141;
+
+        if ( n > 0) {
+            if (n % 2 == 0) {
+                System.out.println("It's a even number");
+            } else {
+                System.out.println("It's a odd number");
+            }
+        }
     }
 }
